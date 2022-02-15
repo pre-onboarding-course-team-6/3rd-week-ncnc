1031ab9753b6392bcfe5ff0cf736f18eeddb2093
gilmujjang <alsanrlf@naver.com>
2022-02-16 04:14:51 +0900

    refactor: 변수 상수화

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,11 +1,29 @@
 /* eslint-disable quotes */
+const MENU = {
+  니콘내콘: "니콘내콘",
+  마이페이지: "마이페이지",
+  고객센터: "고객센터",
+};
+
+const CATEGORY = {
+  땡철이: "땡철이",
+  카페: "카페",
+};
+
+const BRAND = {
+  엔제리너스커피: "엔제리너스커피",
+  스타벅스: "스타벅스",
+};
+
+const HOME = "http://localhost:3000/";
+
 describe("Cypress E2E test start", () => {
   describe("Home page render test", () => {
     beforeEach(() => {
       cy.visit("/");
     });
     it("Home page AppBar test", () => {
-      cy.get(".AppBar").contains("니콘내콘");
+      cy.get(".AppBar").contains(MENU.니콘내콘);
       cy.get(".AppBar > div > button").should("have.class", "MenuIcon");
     });
     it("Home page Component render test", () => {
@@ -21,16 +39,16 @@ describe("Cypress E2E test start", () => {
       cy.get(".moreCompanyInfo").should("not.exist");
     });
     it("Category link test", () => {
-      cy.contains("땡철이").click();
-      cy.url().should("eq", "http://localhost:3000/categories/1");
-      cy.get(".AppBar").contains("땡철이");
+      cy.contains(CATEGORY.땡철이).click();
+      cy.url().should("eq", `${HOME}categories/1`);
+      cy.get(".AppBar").contains(CATEGORY.땡철이);
     });
     it("My page render test", () => {
       cy.get(".MenuIcon").click();
-      cy.get(".AppBar").contains("마이페이지");
+      cy.get(".AppBar").contains(MENU.마이페이지);
       cy.get(".AppBar > div > button").should("have.class", "XIcon");
-      cy.contains("고객센터").click();
-      cy.url().should("eq", "http://localhost:3000/contacts");
+      cy.contains(MENU.고객센터).click();
+      cy.url().should("eq", `${HOME}contacts`);
     });
   });
 
@@ -39,9 +57,9 @@ describe("Cypress E2E test start", () => {
       cy.visit("/contacts");
     });
     it("Contacts page AppBar test", () => {
-      cy.get(".AppBar").contains("고객센터");
+      cy.get(".AppBar").contains(MENU.고객센터);
       cy.get(".AppBar > div > button").should("have.class", "XIcon").click();
-      cy.url().should("have.eq", "http://localhost:3000/");
+      cy.url().should("have.eq", HOME);
     });
     it("1:1 kakao channel link test", () => {
       cy.get(".kakaoChannel")
@@ -52,15 +70,15 @@ describe("Cypress E2E test start", () => {
       cy.get(".FAQBox > div > button").should(($btn) => {
         expect($btn).to.have.length(2);
       });
-      cy.get(".FAQList > div:first > button").click();
-      cy.get(".FAQList > div:first > div").should("be.visible");
-      cy.get(".FAQList > div:first > button").click();
-      cy.get(".FAQList > div:first > div").should("not.exist");
+      function ClickFirstAnswer() {
+        cy.get(".FAQList > div:first > button").click();
+        cy.get(".FAQList > div:first > div").should("be.visible");
+        cy.get(".FAQList > div:first > button").click();
+        cy.get(".FAQList > div:first > div").should("not.exist");
+      }
+      ClickFirstAnswer();
       cy.contains("판매").click();
-      cy.get(".FAQList > div:first > button").click();
-      cy.get(".FAQList > div:first > div").should("be.visible");
-      cy.get(".FAQList > div:first > button").click();
-      cy.get(".FAQList > div:first > div").should("not.exist");
+      ClickFirstAnswer();
     });
   });
 
@@ -69,25 +87,25 @@ describe("Cypress E2E test start", () => {
       cy.visit("/categories/1");
     });
     it("Category page AppBar test", () => {
-      cy.get(".AppBar > div > h2").contains("땡철이");
+      cy.get(".AppBar > div > h2").contains(CATEGORY.땡철이);
       cy.visit("/");
-      cy.contains("땡철이").click();
+      cy.contains(CATEGORY.땡철이).click();
       cy.get(".AppBar > div > button").should("have.class", "BackIcon").click();
-      cy.url().should("have.eq", "http://localhost:3000/");
+      cy.url().should("have.eq", HOME);
     });
     it("Category menu link test", () => {
       cy.get(".CategoryMenu > div > div> button").should(($menu) => {
         expect($menu).to.have.length(9);
       });
-      cy.contains("카페").click();
-      cy.url().should("have.eq", "http://localhost:3000/categories/67");
+      cy.contains(CATEGORY.카페).click();
+      cy.url().should("have.eq", `${HOME}categories/67`);
       cy.get(".HomeCategory").should("be.visible");
     });
     it("Category link test", () => {
       cy.get(".HomeCategory > div > div:first > div > a")
         .click()
         .should(($target) => {
-          expect($target.text()).equal("엔제리너스커피");
+          expect($target.text()).equal(BRAND.엔제리너스커피);
         });
       cy.get(".HomeCategory > div > div:first").click();
       cy.url().should("include", "/brands/");
@@ -99,12 +117,12 @@ describe("Cypress E2E test start", () => {
       cy.visit("/brands/63");
     });
     it("Brand page AppBar test", () => {
-      cy.visit("http://localhost:3000/categories/67");
-      cy.contains("스타벅스").click();
-      cy.url().should("have.eq", "http://localhost:3000/brands/63");
-      cy.get(".AppBar").contains("스타벅스");
+      cy.visit("/categories/67");
+      cy.contains(BRAND.스타벅스).click();
+      cy.url().should("have.eq", `${HOME}brands/63`);
+      cy.get(".AppBar").contains(BRAND.스타벅스);
       cy.get(".AppBar > div > button").should("have.class", "BackIcon").click();
-      cy.url().should("have.eq", "http://localhost:3000/categories/67");
+      cy.url().should("have.eq", `${HOME}categories/67`);
     });
     it("Brand page render test", () => {
       cy.get(".ProductList").should("be.visible");
@@ -118,12 +136,12 @@ describe("Cypress E2E test start", () => {
     it("Brand page AppBar test", () => {
       cy.visit("/brands/63");
       cy.get(".ProductList  > div:first").click();
-      cy.url().should("have.eq", "http://localhost:3000/items/137");
+      cy.url().should("have.eq", `${HOME}items/137`);
       cy.get(".AppBar > div > button").should("have.class", "BackIcon").click();
-      cy.url().should("have.eq", "http://localhost:3000/brands/63");
+      cy.url().should("have.eq", `${HOME}brands/63`);
     });
     it("Brand page render test", () => {
-      cy.contains("스타벅스");
+      cy.contains(BRAND.스타벅스);
       cy.contains("카페아메리카노 T");
       cy.contains("3,600");
       cy.get(".SelectOption").click();
