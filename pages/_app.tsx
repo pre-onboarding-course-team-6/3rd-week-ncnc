dfc10ee0d14af6dab371f917095f555dcd9c8892
gilmujjang <alsanrlf@naver.com>
2022-03-05 17:51:19 +0900

    fix: og image 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,10 +15,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
 			<meta property="og:title" content="더블엔씨 과제"/>
 			<meta property="og:image" content="https://play-lh.googleusercontent.com/smN-gJe3ADgCW-F9gEaas-Gc4firGq9IA_2Wt7__KXao0_Xz3xPx5RsCVW77SzIVfD4"/>
 			<meta property="og:description" content="원티드 프리온보딩 프론트엔드코스 더블엔씨 크론코딩입니다"/>
-			<meta property="og:site_name" content="더블엔씨 과제"/>
-			<meta property="og:locale" content="ko"/>
-			<meta property="og:image:width" content="400"/>
-			<meta property="og:image:height" content="250"/>
 			<title>더블엔씨 과제</title>
 		</Head>
 		<GlobalStyle />
