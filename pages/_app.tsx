6cfd6f487c51af377aca1a7a83df70f13dd8c5f6
gilmujjang <alsanrlf@naver.com>
2022-03-05 17:45:57 +0900

    fix: og image 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
 			<meta property="og:type" content="website"/>
 			<meta property="og:url" content="https://eloquent-hopper-416b1b.netlify.app/"/>
 			<meta property="og:title" content="더블엔씨 과제"/>
-			<meta property="og:image" content="public/titleImage.png"/>
+			<meta property="og:image" content="https://play-lh.googleusercontent.com/smN-gJe3ADgCW-F9gEaas-Gc4firGq9IA_2Wt7__KXao0_Xz3xPx5RsCVW77SzIVfD4"/>
 			<meta property="og:description" content="원티드 프리온보딩 프론트엔드코스 더블엔씨 크론코딩입니다"/>
 			<meta property="og:site_name" content="더블엔씨 과제"/>
 			<meta property="og:locale" content="ko"/>
