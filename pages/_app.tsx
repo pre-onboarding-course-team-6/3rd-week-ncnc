7606edff644e4777872ba36de791471e13091260
gilmujjang <alsanrlf@naver.com>
2022-03-05 17:30:55 +0900

    feat:  meta태그 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,15 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
 	<>
 		<Head>
 			<link rel="icon" href="/favicon.ico" />
-			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<meta property="og:type" content="website"/>
+			<meta property="og:url" content="https://eloquent-hopper-416b1b.netlify.app/"/>
+			<meta property="og:title" content="더블엔씨 과제"/>
+			<meta property="og:image" content="public/titleImage.png"/>
+			<meta property="og:description" content="원티드 프리온보딩 프론트엔드코스 더블엔씨 크론코딩입니다"/>
+			<meta property="og:site_name" content="더블엔씨 과제"/>
+			<meta property="og:locale" content="ko"/>
+			<meta property="og:image:width" content="400"/>
+			<meta property="og:image:height" content="250"/>
 			<title>더블엔씨 과제</title>
 		</Head>
 		<GlobalStyle />
